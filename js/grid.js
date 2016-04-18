4b55c10b51c513a73080bf7ba51d461b32beaf6f
Evan Magoni <esm367@gmail.com>
2016-04-18 00:41:15 -0400

    grid stuf

--- js/grid.js
+++ js/grid.js
@@ -1,21 +1,53 @@
-// auto grid layout
-// var msnry = new Masonry( '#grid', {
-var $grid = $('#grid').masonry({
-  columnWidth: 200,
-  gutter: 10,
-  itemSelector: '#grid a'
-});
+$(function() {
+	// auto grid layout
+	// var msnry = new Masonry( '#grid', {
+	var $grid = $('#grid').masonry({
+	  columnWidth: 200,
+	  gutter: 10,
+	  itemSelector: '#grid a'
+	});
 
-// re-layout grid after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry('layout');
-});
+	// re-layout grid after each image loads
+	$grid.imagesLoaded().progress( function() {
+	  $grid.masonry('layout');
+	});
 
-// make image popup box on click image
-$('#grid').magnificPopup({
-  delegate: 'a', // child items selector, by clicking on it popup will open
-  type: 'image',
-  gallery : {
-  	enabled: true
-  }
+	// make image popup box on click image
+	$('#grid').magnificPopup({
+	  delegate: 'a', // child items selector, by clicking on it popup will open
+	  type: 'image',
+	  gallery : {
+	  	enabled: true
+	  }
+	});
+
+	// tags!!!!
+
+	// gather a list of all tags from the tagged images on the page
+	var taggedImages = $('a[data-tags]');
+	var i = taggedImages.length;
+	var currentTag;
+	var allTags = [];
+	while(i--) {
+		currentTag = taggedImages[i].getAttribute('data-tags');
+		if (allTags.indexOf(currentTag) === -1) {
+			allTags.push(currentTag);
+		}
+	}
+
+	// print list of tags
+	var j = allTags.length;
+	var tagsMenu = document.createDocumentFragment();
+	var tagLink;
+	var currentTagName;
+	while (j--) {
+		currentTagName = allTags[j];
+		tagLink = document.createElement('a');
+		tagLink.setAttribute('href', '#' + currentTagName);
+		tagLink.appendChild(document.createTextNode(currentTagName));
+		tagsMenu.appendChild(tagLink);
+	}
+
+	var tagMenuContainer = document.getElementById('tag-menu');
+	tagMenuContainer.appendChild(tagsMenu);
 });
