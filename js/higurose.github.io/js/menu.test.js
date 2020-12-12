c0bbbdadfdc4d97b84897af507d98e73361107e9
higurose <higoons@gmail.com>
2020-12-11 22:23:33 -0500

    Create menu.test.js

---
+++ js/higurose.github.io/js/menu.test.js
@@ -0,0 +1,18 @@
+window.onload = function()
+{
+    var toggleVisible = function(id)
+    {
+         var obj = document.getElementById(id);
+         if (obj.style.display === 'none')
+            obj.style.display = 'block';
+         else
+            obj.style.display = 'none';
+    };
+
+    
+    document.getElementById('zine').onclick = function() { toggleVisible('zinemenu'); };
+    document.getElementById('zinemenu').style.display = 'none';
+    
+        document.getElementById('il').onclick = function() { toggleVisible('ilmenu'); };
+    document.getElementById('ilmenu').style.display = 'none';
+};
